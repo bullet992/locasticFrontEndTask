0f6f98e2db8f86400958667fd0f1c7d8e7906b6e
bullet992 <octeamcroatia@gmail.com>
2018-01-18 22:30:03 +0100

    Added slideshow (css, js, images)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,14 @@
 import 'normalize.css';
 import './../css/style.scss';
 import './navbar.js';
+import './responsiveslides.js';
+
+import firstSlide from '../images/1.jpg';
+import secondSlide from '../images/2.jpg';
+import thirdSlide from '../images/3.jpg';
+var img1 = document.getElementById('firstSlide');
+img1.src = firstSlide;
+var img2 = document.getElementById('secondSlide');
+img2.src = secondSlide;
+var img3 = document.getElementById('thirdSlide');
+img3.src = thirdSlide;
